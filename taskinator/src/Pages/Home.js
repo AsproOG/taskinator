9a1f5a1e3332b3a10719176d8f97d346d0a1df55
AsproOG <116924836+AsproOG@users.noreply.github.com>
2023-06-08 17:18:32 -0400

    video main added

--- taskinator/src/Pages/Home.js
+++ taskinator/src/Pages/Home.js
@@ -1,4 +1,24 @@
-export default function Home(){
-return <h1> Home is Work in Progress</h1>
 
+
+import homeVideo from './video.mp4'
+import logo from './TaskinatorWOL.png'
+export default function Home(){
+return (
+
+<home className="homePage">
+<video autoPlay muted loop className="background-video">
+  <source src={homeVideo} type="video/mp4" />
+
+</video>
+<div className="content">
+<h1> Our vision is to be the place for companies to find the best of the 
+    best students through tasks listed by them.
+     If a student can complete the rigorous tasks, they deserve to be hired.
+     </h1>
+  
+  
+     
+      </div>
+    </home>
+  );
 }
