8be9b305ef5962eaff11c17e867929c6db419acd
AsproOG <116924836+AsproOG@users.noreply.github.com>
2023-06-07 20:36:35 -0400

    Almost working NavBar
    The nav bar is almost working. App is set up, just need to fix the links so that onClick, the user is put on the page that is intended

--- taskinator/src/App.js
+++ taskinator/src/App.js
@@ -1,10 +1,36 @@
 import Navbar from "./Navbar"
 import "./styles.css"
 
+import About from "./Pages/About"
+import CompanyList from "./Pages/CompanyList"
+import SignIn from "./Pages/SignIn"
+
 
 function App() {
+  let component
+  switch (window.location.pathname){
+   
+
+    case "/About":
+        component = <About />
+      break
+    case "/CompanyList":
+        component = <CompanyList />
+      break
+    case "/SignIn":
+        component = <SignIn />
+        break
+  }
   return (
+   
+    <>
       <Navbar />
+      {component}
+
+      
+      </>
+
+      
   )
 }
 
--- taskinator/src/Navbar.js
+++ taskinator/src/Navbar.js
@@ -6,17 +6,20 @@ return (
 <nav className ="nav"> 
 
 
-< img src= {logo} alt= "Taskin8r logo"/>
+< img src= {logo}  alt= "Taskin8r logo"/>
     
  <ul> 
     <li>
     <a href= "/about"> ABOUT </a>
-          </li>
-          <li>
+    </li>
+
+    <li>
     <a href= "/companyList"> COMPANY LIST</a>
-        </li>
-        <li> <a href= "/signIn"> SIGN IN / SIGN UP </a>
-        </li>
+    </li>
+
+    <li> 
+        <a href= "/signIn"> SIGN IN / SIGN UP </a>
+    </li>
  </ul>
 </nav>
   
