7f4d2b897a4b2d14c00e67d1513bd743dc2054b7
RafinalHaque <99057380+RafinalHaque@users.noreply.github.com>
2023-06-08 17:21:00 -0400

    y

--- taskinator/src/Pages/SignIn.js
+++ taskinator/src/Pages/SignIn.js
@@ -1,4 +1,19 @@
-export default function SignIn(){
-    return <h1> Sign in is Work in Progress</h1>
-    
-    }
+import { Button } from "@chakra-ui/react";
+
+function StudentButton() {
+  return <Button>Student</Button>;
+}
+
+function CompanyButton() {
+  return <Button>Company</Button>;
+}
+
+export default function SignIn() {
+  return (
+    <div>
+      <h1>Welcome to the Sign in page! Are you a Student or part of a Company?</h1>
+      <StudentButton />
+      <CompanyButton />
+    </div>
+  );
+}
